58bbd8defb0c8e59b15c7e951782fb388b9b3ca1
Vinh Truong <vinhphuc.truong@gmail.com>
2023-09-03 17:48:18 +0300

    macros: Declare private access specifier

--- src/common/macros.hpp
+++ src/common/macros.hpp
@@ -1,10 +1,14 @@
 #pragma once
 
 #define FB_NO_COPY(T)     \
+                          \
+private:                  \
     T(const T&) = delete; \
     auto operator=(const T&)->T& = delete;
 
 #define FB_NO_COPY_MOVE(T)                 \
+                                           \
+private:                                   \
     T(const T&) = delete;                  \
     T(T&&) = delete;                       \
     auto operator=(const T&)->T& = delete; \
