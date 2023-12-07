31510be63489a2fcda9f5102f29c4746a533bc75
Vinh Truong <vinhphuc.truong@gmail.com>
2023-12-07 20:33:08 +0200

    Increase `MAX_RTV_DESCRIPTOR`

--- src/kitchen/gpu/descriptors.hpp
+++ src/kitchen/gpu/descriptors.hpp
@@ -54,7 +54,7 @@ class GpuDescriptors {
 public:
     static constexpr uint MAX_CBV_SRV_UAV_DESCRIPTOR = 256;
     static constexpr uint MAX_SAMPLER_DESCRIPTOR = 16;
-    static constexpr uint MAX_RTV_DESCRIPTOR = 16;
+    static constexpr uint MAX_RTV_DESCRIPTOR = 32;
     static constexpr uint MAX_DSV_DESCRIPTOR = 16;
 
     GpuDescriptors() = default;
