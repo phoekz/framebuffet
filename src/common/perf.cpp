81a1f6279e5553259b8010840330926b93a62ee7
Vinh Truong <vinhphuc.truong@gmail.com>
2024-09-01 10:35:58 +0300

    tracy: Fix executable name

--- src/common/perf.cpp
+++ src/common/perf.cpp
@@ -5,7 +5,7 @@
 namespace fb {
 
 auto tracy_capture() -> void {
-    const wchar_t* path = L"Tracy.exe";
+    const wchar_t* path = L"tracy-profiler.exe";
     const wchar_t* args = L"-a 127.0.0.1";
     ShellExecuteW(nullptr, L"open", path, args, nullptr, SW_SHOW);
 }
