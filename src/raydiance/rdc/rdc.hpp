7bf3383580be3bf6d6076de260fabbe9577f3da9
Vinh Truong <vinhphuc.truong@gmail.com>
2024-10-21 16:27:03 +0900

    WIP_RDC: primary rays

---
+++ src/raydiance/rdc/rdc.hpp
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <common/common.hpp>
+#include <kitchen/kitchen.hpp>
+#include <baked/raydiance/baked.hpp>
+
+#include "geometry.hpp"
+
+namespace fb {
+
+auto rdc_render(const baked::raydiance::Assets& assets, const baked::raydiance::Shaders& shaders)
+    -> void;
+
+} // namespace fb
