cc9af26790238132941f371de9af8678991ee349
Vinh Truong <vinhphuc.truong@gmail.com>
2024-10-27 11:48:28 +0200

    WIP_RDC: Fix shading

--- src/baked/raydiance/baked.cpp
+++ src/baked/raydiance/baked.cpp
@@ -8,10 +8,10 @@ namespace fb::baked::raydiance {
     }
 
 auto Assets::load() -> void {
-    // hash: d128ad8e88a6aa70b81966c2c0497d47
+    // hash: 29c37c34b03248980cfb4b4b17397278
     FB_PERF_FUNC();
     _data = read_whole_file("fb_raydiance_assets.bin");
-    FB_ASSERT(_data.size() == 300132);
+    FB_ASSERT(_data.size() == 310832);
 }
 
 auto Assets::cube_mesh() const -> Mesh {
@@ -67,9 +67,9 @@ auto Assets::sphere_mesh() const -> Mesh {
 }
 
 auto Assets::rounded_cube_mesh() const -> Mesh {
-    // vertex_count: 2092
-    // face_count: 2862
-    // submesh_count: 2
+    // vertex_count: 2088
+    // face_count: 2860
+    // submesh_count: 1
     return Mesh {
         .transform = float4x4(
             // clang-format off
@@ -83,31 +83,26 @@ auto Assets::rounded_cube_mesh() const -> Mesh {
             0x0p+0f, 0x0p+0f, 0x0p+0f, 0x1p+0f
             // clang-format on
         ),
-        // hash: 4c3f6d2f77bd50dbe8802b12618da099
-        .vertices = transmuted_span<Vertex>(154200, 2092),
-        // hash: 022559a91fe6658119a12308588e710c
-        .indices = transmuted_span<Index>(254616, 8586),
-        // hash: 63483ff816dab7a6a3cfefe47ed35bef
-        .submeshes = transmuted_span<Submesh>(288960, 2),
+        // hash: a32f5011e497cf1f85dfb48ead9ca401
+        .vertices = transmuted_span<Vertex>(154200, 2088),
+        // hash: 64edff1faf8cb95f1c8c921871c0398b
+        .indices = transmuted_span<Index>(254424, 8580),
+        // hash: f0d8ade6012fb22abe1567d2bd1897cf
+        .submeshes = transmuted_span<Submesh>(288744, 1),
     };
 }
 
 auto Assets::rounded_cube_base_color_texture() const -> Texture {
     decltype(Texture::datas) datas = {};
     // clang-format off
-    datas[ 0] = texture_data( 128,    4096,    288984,    4096); // hash: bb15d9fb8c01e5c221dcc0af48dfd89d, width: 32, height: 32
-    datas[ 1] = texture_data(  64,    1024,    293080,    1024); // hash: 495ebb7f37bcf8f373906355a2f4ef88, width: 16, height: 16
-    datas[ 2] = texture_data(  32,     256,    294104,     256); // hash: fd2547ab7e9275382a8ffbc654278c10, width: 8, height: 8
-    datas[ 3] = texture_data(  16,      64,    294360,      64); // hash: 33008f52ca62e5748a12728a8b61b354, width: 4, height: 4
-    datas[ 4] = texture_data(   8,      16,    294424,      16); // hash: 2bfe8639ccc553f2b51dc54c3d500160, width: 2, height: 2
-    datas[ 5] = texture_data(   4,       4,    294440,       4); // hash: eb0bdd519710d6f6e15ee063eda5ac5e, width: 1, height: 1
+    datas[ 0] = texture_data(   4,       4,    288756,       4); // hash: 27f693aa771c16015466d1025f82aca2, width: 1, height: 1
     // clang-format on
     return Texture {
         .format = DXGI_FORMAT_R8G8B8A8_UNORM_SRGB,
-        .width = 32,
-        .height = 32,
+        .width = 1,
+        .height = 1,
         .channel_count = 4,
-        .mip_count = 6,
+        .mip_count = 1,
         .datas = datas,
     };
 }
@@ -136,31 +131,32 @@ auto Assets::plane_mesh() const -> Mesh {
             0x0p+0f, 0x0p+0f, 0x0p+0f, 0x1p+0f
             // clang-format on
         ),
-        // hash: 06bd69f738d4d2966608ee7af94fb31d
-        .vertices = transmuted_span<Vertex>(294444, 4),
+        // hash: fc0bfb437fbe4b7bde660d46d2d9c053
+        .vertices = transmuted_span<Vertex>(288760, 4),
         // hash: e61613adcfc67d4d78864c88896b5ba0
-        .indices = transmuted_span<Index>(294636, 6),
+        .indices = transmuted_span<Index>(288952, 6),
         // hash: c98593cf018afad07e4bedad79142b75
-        .submeshes = transmuted_span<Submesh>(294660, 1),
+        .submeshes = transmuted_span<Submesh>(288976, 1),
     };
 }
 
 auto Assets::plane_texture() const -> Texture {
     decltype(Texture::datas) datas = {};
     // clang-format off
-    datas[ 0] = texture_data( 128,    4096,    294672,    4096); // hash: 4bcf48d6be3640293c4f4c26a9d64a48, width: 32, height: 32
-    datas[ 1] = texture_data(  64,    1024,    298768,    1024); // hash: 4cb77aebbb465cbcbf17135c3e816787, width: 16, height: 16
-    datas[ 2] = texture_data(  32,     256,    299792,     256); // hash: 91699b3869dc3e30252a841aac190ec4, width: 8, height: 8
-    datas[ 3] = texture_data(  16,      64,    300048,      64); // hash: fa1572b701b6ed624dfe94358604f0eb, width: 4, height: 4
-    datas[ 4] = texture_data(   8,      16,    300112,      16); // hash: a77803af8e3526796b1a201bc1f800ba, width: 2, height: 2
-    datas[ 5] = texture_data(   4,       4,    300128,       4); // hash: 18a31d7b8588932d35a7129091da92d3, width: 1, height: 1
+    datas[ 0] = texture_data( 256,   16384,    288988,   16384); // hash: da2d47e127df9fdc084135968d646891, width: 64, height: 64
+    datas[ 1] = texture_data( 128,    4096,    305372,    4096); // hash: 7f20e38d125579b8440ea035c9147374, width: 32, height: 32
+    datas[ 2] = texture_data(  64,    1024,    309468,    1024); // hash: a419af177b1b69bb6e22a51392a4e806, width: 16, height: 16
+    datas[ 3] = texture_data(  32,     256,    310492,     256); // hash: c2fbbe9ab9467d3f4998ea1f82473e95, width: 8, height: 8
+    datas[ 4] = texture_data(  16,      64,    310748,      64); // hash: fa1572b701b6ed624dfe94358604f0eb, width: 4, height: 4
+    datas[ 5] = texture_data(   8,      16,    310812,      16); // hash: a77803af8e3526796b1a201bc1f800ba, width: 2, height: 2
+    datas[ 6] = texture_data(   4,       4,    310828,       4); // hash: 18a31d7b8588932d35a7129091da92d3, width: 1, height: 1
     // clang-format on
     return Texture {
         .format = DXGI_FORMAT_R8G8B8A8_UNORM,
-        .width = 32,
-        .height = 32,
+        .width = 64,
+        .height = 64,
         .channel_count = 4,
-        .mip_count = 6,
+        .mip_count = 7,
         .datas = datas,
     };
 }
--- src/raydiance/rdc/rdc.cpp
+++ src/raydiance/rdc/rdc.cpp
@@ -33,46 +33,59 @@ auto rdc_render(const baked::raydiance::Assets& assets, const baked::raydiance::
     // Test raytracing.
     {
         std::vector<RdcTriangle> triangles;
-        // const auto& mesh = assets.cube_mesh();
-        // const auto& mesh = assets.sphere_mesh();
-        // const auto& mesh = assets.rounded_cube_mesh();
-        const auto& mesh = assets.plane_mesh();
-        for (const auto& submesh : mesh.submeshes) {
-            const auto vertices = mesh.vertices;
-            const auto indices = mesh.indices;
-            const auto transform = FLOAT4X4_IDENTITY;
-            for (uint i = submesh.start_index; i < submesh.index_count; i += 3) {
-                const auto i0 = indices[i + 0];
-                const auto i1 = indices[i + 1];
-                const auto i2 = indices[i + 2];
-                const auto& v0 = vertices[i0];
-                const auto& v1 = vertices[i1];
-                const auto& v2 = vertices[i2];
-                RdcTriangle triangle;
-                triangle.positions[0] = float4x4_transform_point(transform, v0.position);
-                triangle.positions[1] = float4x4_transform_point(transform, v1.position);
-                triangle.positions[2] = float4x4_transform_point(transform, v2.position);
-                triangle.texcoords[0] = v0.texcoord;
-                triangle.texcoords[1] = v1.texcoord;
-                triangle.texcoords[2] = v2.texcoord;
-                triangle.normals[0] = v0.normal;
-                triangle.normals[1] = v1.normal;
-                triangle.normals[2] = v2.normal;
-                triangles.push_back(triangle);
+        const auto& cube_mesh = assets.rounded_cube_mesh();
+        const auto& plane_mesh = assets.plane_mesh();
+        enum class MaterialId : uint {
+            Cube,
+            Plane,
+        };
+        struct MeshMaterialId {
+            const baked::Mesh& mesh;
+            MaterialId material;
+        };
+        for (const auto& [mesh, material] : {
+                 MeshMaterialId {cube_mesh, MaterialId::Cube},
+                 MeshMaterialId {plane_mesh, MaterialId::Plane},
+             }) {
+            for (const auto& submesh : mesh.submeshes) {
+                const auto vertices = mesh.vertices;
+                const auto indices = mesh.indices;
+                const auto transform = FLOAT4X4_IDENTITY;
+                for (uint i = submesh.start_index; i < submesh.index_count; i += 3) {
+                    const auto i0 = indices[i + 0];
+                    const auto i1 = indices[i + 1];
+                    const auto i2 = indices[i + 2];
+                    const auto& v0 = vertices[i0];
+                    const auto& v1 = vertices[i1];
+                    const auto& v2 = vertices[i2];
+                    RdcTriangle triangle;
+                    triangle.positions[0] = float4x4_transform_point(transform, v0.position);
+                    triangle.positions[1] = float4x4_transform_point(transform, v1.position);
+                    triangle.positions[2] = float4x4_transform_point(transform, v2.position);
+                    triangle.texcoords[0] = v0.texcoord;
+                    triangle.texcoords[1] = v1.texcoord;
+                    triangle.texcoords[2] = v2.texcoord;
+                    triangle.normals[0] = v0.normal;
+                    triangle.normals[1] = v1.normal;
+                    triangle.normals[2] = v2.normal;
+                    triangle.material = (uint)material;
+                    triangles.push_back(triangle);
+                }
             }
         }
 
         std::vector<RdcBvhNode> nodes;
         rdc_bvh_create(nodes, triangles);
 
-        auto texture = assets.plane_texture();
+        const auto textures =
+            std::to_array({assets.rounded_cube_base_color_texture(), assets.plane_texture()});
 
         const auto sun_azimuth = 0.0f;                  // [0, 360].
-        const auto sun_elevation = rad_from_deg(30.0f); // [0, 90].
+        const auto sun_elevation = rad_from_deg(45.0f); // [0, 90].
         RdcSkyState sky;
         RdcSkyParams sky_params;
         sky_params.elevation = sun_elevation;
-        sky_params.turbidity = 1;
+        sky_params.turbidity = 3;
         sky_params.albedo[0] = 1.0f;
         sky_params.albedo[1] = 1.0f;
         sky_params.albedo[2] = 1.0f;
@@ -86,15 +99,21 @@ auto rdc_render(const baked::raydiance::Assets& assets, const baked::raydiance::
             std::sin(sun_angle_y) * std::sin(sun_angle_xz)
         );
 
-        const auto exposure = exposure_from_stops(4.0f);
+        const auto exposure = exposure_from_stops(5.0f);
 
-        const auto sample_count = 32u;
-        const auto bounce_count = 4u;
+        const auto sample_count = 16u;
+        const auto bounce_count = 5u;
         const auto image_scale = 25u;
         const auto image_size = uint2(16u * image_scale, 10u * image_scale);
         const auto image_aspect = (float)image_size.x / (float)image_size.y;
         const auto camera_fovy = rad_from_deg(60.0f);
-        const auto camera_position = float3(1.5f, 1.0f, 1.5f);
+        const auto camera_angle_x = rad_from_deg(-30.0f);
+        const auto camera_distance = 2.0f;
+        const auto camera_position = float3(
+            camera_distance * std::cos(camera_angle_x),
+            1.5f,
+            camera_distance * std::sin(camera_angle_x)
+        );
         const auto view_from_world =
             float4x4_lookat(camera_position, float3(0.0f), float3(0.0f, 1.0f, 0.0f));
         const auto clip_from_view = float4x4_perspective(camera_fovy, image_aspect, 0.1f, 100.0f);
@@ -107,6 +126,7 @@ auto rdc_render(const baked::raydiance::Assets& assets, const baked::raydiance::
         auto ray_count = 0u;
         for (uint pixel_y = 0u; pixel_y < image_size.y; pixel_y++) {
             for (uint pixel_x = 0u; pixel_x < image_size.x; pixel_x++) {
+                // Estimate radiance.
                 float3 sum_radiance = FLOAT3_ZERO;
                 for (uint sample_idx = 0u; sample_idx < sample_count; sample_idx++) {
                     // Primary ray.
@@ -142,7 +162,8 @@ auto rdc_render(const baked::raydiance::Assets& assets, const baked::raydiance::
                             );
 
                             // Sample texture.
-                            const auto texel = rdc_texture_sample(texture, texcoord);
+                            const auto texel =
+                                rdc_texture_sample(textures[triangle.material], texcoord);
                             const auto texel_rgb = float3(texel.x, texel.y, texel.z);
 
                             // Sample next direction.
@@ -159,7 +180,7 @@ auto rdc_render(const baked::raydiance::Assets& assets, const baked::raydiance::
                             ray.dir = wi_world;
 
                             // Shading.
-                            const auto cos_theta = std::abs(float3_dot(wo_world, normal));
+                            const auto cos_theta = std::abs(float3_dot(wi_world, normal));
                             throughput *= sample.r * cos_theta / sample.pdf;
 
                             hit_count++;
@@ -192,11 +213,17 @@ auto rdc_render(const baked::raydiance::Assets& assets, const baked::raydiance::
                         tonemap_aces(exposure * sum_radiance.y),
                         tonemap_aces(exposure * sum_radiance.z)
                     );
+                    FB_ASSERT(linear_color.x >= 0.0f && linear_color.x <= 1.0f);
+                    FB_ASSERT(linear_color.y >= 0.0f && linear_color.y <= 1.0f);
+                    FB_ASSERT(linear_color.z >= 0.0f && linear_color.z <= 1.0f);
                     const auto srgb_color = float3(
                         srgb_from_linear(linear_color.x),
                         srgb_from_linear(linear_color.y),
                         srgb_from_linear(linear_color.z)
                     );
+                    FB_ASSERT(srgb_color.x >= 0.0f && srgb_color.x <= 1.0f);
+                    FB_ASSERT(srgb_color.y >= 0.0f && srgb_color.y <= 1.0f);
+                    FB_ASSERT(srgb_color.z >= 0.0f && srgb_color.z <= 1.0f);
                     const RgbaByte color = {
                         (uint8_t)(255.0f * srgb_color.x),
                         (uint8_t)(255.0f * srgb_color.y),
@@ -205,13 +232,22 @@ auto rdc_render(const baked::raydiance::Assets& assets, const baked::raydiance::
                     };
                     const uint px = pixel_x;
                     const uint py = image_size.y - pixel_y - 1;
-                    const uint pixel_idx = py * image_size.x + px;
+                    const uint pixel_idx = px + py * image_size.x;
                     image[pixel_idx] = color;
                 }
             }
         }
-        stbi_write_png("rdc_render.png", image_size.x, image_size.y, 4, image.data(), 0);
-        FB_LOG_INFO("hit_count={} ray_count={}", hit_count, ray_count);
+
+        const auto image_path = make_absolute_path("rdc_render.png");
+        stbi_write_png(
+            image_path.c_str(),
+            image_size.x,
+            image_size.y,
+            4,
+            image.data(),
+            4 * image_size.x
+        );
+        FB_LOG_INFO("path={}, hit_count={}, ray_count={}", image_path, hit_count, ray_count);
 
         if (1) {
             exit(0);
