69899904330a40319060dc93366b8c606f39578f
Vinh Truong <vinhphuc.truong@gmail.com>
2025-11-09 14:46:26 +0200

    No copy/move macros don't need to specify access specifiers

--- src/common/macros.hpp
+++ src/common/macros.hpp
@@ -3,14 +3,10 @@
 #define FB_INLINE __forceinline
 
 #define FB_NO_COPY(T)     \
-                          \
-private:                  \
     T(const T&) = delete; \
     auto operator=(const T&)->T& = delete;
 
 #define FB_NO_COPY_MOVE(T)                 \
-                                           \
-private:                                   \
     T(const T&) = delete;                  \
     T(T&&) = delete;                       \
     auto operator=(const T&)->T& = delete; \
